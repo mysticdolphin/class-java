99227bf4ca5624d46822864bf3a1b5357e2d4900
mysticdolphin <43502323+mysticdolphin@users.noreply.github.com>
2018-09-22 12:30:13 -0700

    1st Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main (String [] args) {
+    String iHateCoding = "Hello World";
+    System.out.println(iHateCoding);
+    
+    int num = 9;
+    int num2 = 8;
+    System.out.println (((double) num)/num2);
+  }
+}
